1eae363c21b79ba06b30cb8271c153abeec29e27
guille <info@guillegonzarod.com>
2021-12-28 11:33:40 +0100

    Bind the UI elements in MainActivity

--- app/src/main/java/com/guillegonzarod/smartgreeting/MainActivity.kt
+++ app/src/main/java/com/guillegonzarod/smartgreeting/MainActivity.kt
@@ -2,8 +2,20 @@ package com.guillegonzarod.smartgreeting
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Switch
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    // UI
+    val editTextGreeting = findViewById<EditText>(R.id.editTextGreeting)
+    val switchCaps = findViewById<Switch>(R.id.switchCaps)
+    val textViewCaps = findViewById<TextView>(R.id.textViewCaps)
+    val textViewGreeting = findViewById<TextView>(R.id.textViewGreeting)
+    val buttonGreeting = findViewById<Button>(R.id.buttonGreeting)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
