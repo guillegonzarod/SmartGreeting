50ccc15be1b8dfa58240854305bc2d2c46e568b9
guille <info@guillegonzarod.com>
2021-12-28 11:00:57 +0100

    Bind the UI elements in ViewController

--- SmartGreeting/ViewController.swift
+++ SmartGreeting/ViewController.swift
@@ -9,6 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // UI
+    @IBOutlet weak var editTextGreeting: UITextField!
+    @IBOutlet weak var switchCaps: UISwitch!
+    @IBOutlet weak var textViewCaps: UILabel!
+    @IBOutlet weak var textViewGreeting: UILabel!
+    @IBOutlet weak var buttonGreeting: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
